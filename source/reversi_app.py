ba85e1ff37e80125566d527ddfe59b333ad81615
na0ki_mac <wehallo53.en@gmail.com>
2022-11-18 22:42:34 +0900

    置く処理まで

--- source/reversi_app.py
+++ source/reversi_app.py
@@ -20,11 +20,115 @@ from source import pytorch_pretrained
 def checkWin(board):
    pass
 
-player_one="●"
-player_two="○"
+NUM_SQUARE=8#6
 
-board_num=8#6
-desp_cols=[0.2]*(board_num-1)+[0.9]#列の間隔　一番右だけ大きくするといい感じに左側に固まる！
+YOUR_COLOR = "●" # あなたの石の色
+COM_COLOR = "○" # 相手の石の色
+PLACABLE_COLOR = '□' # 次に石を置ける場所を示す色
+BOARD_COLOR="＿"
+# 角度の設定
+base_theta = np.pi / 2
+# 70度
+theta_0 = np.pi / (180 / 70)
+
+# プレイヤーを示す値
+YOU = 1
+COM = 2
+
+color_dic = { # 石の色を保持する辞書
+            YOU : YOUR_COLOR,
+            COM : COM_COLOR
+        }
+
+def registerAngle(x, y, which):
+        '''新しい石を置いたときに角度を登録する'''
+        if which == YOU:
+            st.session_state.angle[y][x] += base_theta
+        else:
+            st.session_state.angle[y][x] -= base_theta
+
+ 
+
+def oku(r,c,which):
+    #drawDisk
+    st.session_state.board[r][c]=color_dic[which]
+    registerAngle(r,c,which)
+
+def checkPlacable(x, y):
+        '''(x,y)に石が置けるかどうかをチェック'''
+        # その場所に石が置かれていれば置けない
+        if st.session_state.board[x][y] == YOUR_COLOR or st.session_state.board[x][y] == COM_COLOR:
+            return False
+
+        if st.session_state.next_player == YOU:
+            other = COM
+        else:
+            other = YOU
+
+        # (x,y)座標から縦横斜め全方向に対して相手の石が裏返せるかどうかを確認
+        for j in range(-1, 2):
+            for i in range(-1, 2):
+
+                # 真ん中方向はチェックしてもしょうがないので次の方向の確認に移る
+                if i == 0 and j == 0:
+                    continue
+
+                # その方向が盤面外になる場合も次の方向の確認に移る
+                if x + i < 0 or x + i >= NUM_SQUARE or y + j < 0 or y + j >= NUM_SQUARE:
+                    continue
+
+                # 隣が相手の色でなければその方向に石を置いても裏返せない
+                if st.session_state.board[y + j][x + i] != color_dic[other]:
+                    continue
+
+                # 置こうとしているマスから遠い方向へ１マスずつ確認
+                for s in range(2, NUM_SQUARE):
+                    # 盤面外のマスはチェックしない
+                    if x + i * s >= 0 and x + i * s < NUM_SQUARE and y + j * s >= 0 and y + j * s < NUM_SQUARE:
+                        
+                        if st.session_state.board[y + j * s][x + i * s] == None:
+                            # 自分の石が見つかる前に空きがある場合
+                            # この方向の石は裏返せないので次の方向をチェック
+                            break
+
+                        # その方向に自分の色の石があれば石が裏返せる
+                        if st.session_state.board[y + j * s][x + i * s] == color_dic[st.session_state.next_player]:
+                            return True
+        
+        # 裏返せる石がなかったので(x,y)に石は置けない
+        return False
+def showPlacable(placable):
+    '''placableに格納された次に石が置けるマスの色を変更する'''
+
+    for x in range(NUM_SQUARE):
+        for y in range(NUM_SQUARE):
+
+            # fillを変更して石が置けるマスの色を変更
+            # その場所に石が置かれていれば置けない
+            
+            if st.session_state.board[x][y] == YOUR_COLOR or st.session_state.board[x][y] == COM_COLOR:
+                continue
+            
+            if (x, y) in placable:
+                st.session_state.board[x][y] = PLACABLE_COLOR
+            else:
+                st.session_state.board[x][y] = BOARD_COLOR
+
+def getPlacable():
+    '''次に置くことができる石の位置を取得'''
+
+    placable = []
+
+    for y in range(NUM_SQUARE):
+        for x in range(NUM_SQUARE):
+            # (x,y) の位置のマスに石が置けるかどうかをチェック
+            if checkPlacable(x, y):
+                # 置けるならその座標をリストに追加
+                placable.append((x, y))
+    return placable
+
+init_message=PLACABLE_COLOR+"の場所におけます。"
+desp_cols=[0.2]*(NUM_SQUARE-1)+[0.9]#列の間隔　一番右だけ大きくするといい感じに左側に固まる！
 def main():
     st.header('Q-Reversi')
     st.caption("[QC4U](https://altema.is.tohoku.ac.jp/QC4U/) Group4(Quantum Force)")
@@ -34,26 +138,42 @@ def main():
 
     # Initialize state.
     if "board" not in st.session_state:
-        st.session_state.board = np.full((board_num, board_num), "＿", dtype=str)
-        mannaka=int(board_num/2)
-        st.session_state.board[mannaka][mannaka]=player_one
-        st.session_state.board[mannaka-1][mannaka-1]=player_one
-        st.session_state.board[mannaka][mannaka-1]=player_two
-        st.session_state.board[mannaka-1][mannaka]=player_two
-        st.session_state.next_player =player_two
+        st.session_state.board = np.full((NUM_SQUARE, NUM_SQUARE),BOARD_COLOR, dtype=str)
+        # 石の角度を管理する2次元リストを作成（最初は90度（重ね合わせ））
+        st.session_state.angle = np.full((NUM_SQUARE, NUM_SQUARE), np.pi / 2, dtype=float)
+        mannaka=int(NUM_SQUARE/2)
+        oku(mannaka,mannaka,YOU)
+        oku(mannaka-1,mannaka-1,YOU)
+        oku(mannaka-1,mannaka,COM)
+        oku(mannaka,mannaka-1,COM)
+        st.session_state.next_player =COM 
         st.session_state.winner = None
-    st.write("next player is:"+st.session_state.next_player)
+        st.session_state.placable = None
+        st.session_state.message=init_message
+    st.write("next player is:"+color_dic[st.session_state.next_player])
+    st.write(st.session_state.message)
+
         # Define callbacks to handle button clicks.
+    st.session_state.placable = getPlacable()
+    showPlacable(st.session_state.placable)
+
     def handle_click(i, j):
         if not st.session_state.winner:
-            # TODO: Handle the case when nobody wins but the game is over!
-            st.session_state.board[i, j] = st.session_state.next_player
-            st.session_state.next_player = (
-               player_one if st.session_state.next_player ==player_two else player_two
-            )
-            winner = checkWin(st.session_state.board)
-            if winner != "＿":
-                st.session_state.winner = winner
+            if checkPlacable(i, j):
+                st.session_state.message=init_message
+                # TODO: Handle the case when nobody wins but the game is over!
+                st.session_state.board[i, j] = color_dic[st.session_state.next_player]
+
+                st.session_state.next_player = (
+                YOU  if st.session_state.next_player ==COM  else COM
+                )
+                winner = checkWin(st.session_state.board)
+                if winner != BOARD_COLOR:
+                    st.session_state.winner = winner
+            else:
+                print("a")
+                st.session_state.message="注意！そこにはおけません"
+                
 
     # Show one button for each field.
     for i, row in enumerate(st.session_state.board):
